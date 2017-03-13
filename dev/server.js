c088231e06d26311a5cfdeca713b487785ccc5e7
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-03-13 18:17:10 +0530

    loads of comments before changes

--- dev/server.js
+++ dev/server.js
@@ -1,79 +1,102 @@
 import express from 'express'
 import React from 'react'
-import {renderToString} from 'react-dom/server'
-import template from '../utils/template'
+import {renderToString} from 'react-dom/instance'
 import fs from 'fs'
 import clearRequire from 'clear-require'
 import {loading, info} from 'prettycli'
 import webpack from 'webpack'
-import config from './webpack.config'
 import {resolve} from 'path'
 import styleSheet from 'styled-components/lib/models/StyleSheet'
 import chokidar from 'chokidar'
+import dependencyTree from 'dependency-tree'
+import template from '../utils/template'
+import config from './webpack.config'
 
-var dependencyTree = require('dependency-tree')
-
-const compiler = webpack(config)
-
-loading('PAGES', 'Defining routes...')
-let pages = {}
+/* generate dependency tree */
 let tree = {}
-
 const generateTree = files => files.map(file => tree[file] = dependencyTree.toList({
   filename: resolve(`./pages/${file}`),
   directory: '/'
 }))
 
+/* generate pages cache */
+let pages = {}
 const getPages = () => {
   const files = fs.readdirSync('./pages')
   files.filter(file => file.endsWith('.js'))
   .map(file => pages[file.replace('.js', '')] = require(resolve(`./pages/${file}`)))
   generateTree(files)
 }
+
+loading('PAGES', 'Defining routes...')
 getPages()
 
+/* uncache dependency tree on change */
 const uncache = path => {
   const roots = Object.keys(tree)
   roots.map(root => {
+    /* find matching trees */
     const index = tree[root].indexOf(resolve(path))
     if (index !== -1) {
+      /* get components up the tree */
       const stale = tree[root].slice(index)
+      /* remove from require cache */
       stale.map(component => clearRequire(resolve(component)))
     }
   })
-  /* Create pages again */
-  getPages()
+  getPages() // generate page cache again
 }
+
+/* define watchers */
 
 let watcher = chokidar.watch('**/*.js', {ignored: 'node_modules'})
 watcher.on('change', uncache).on('unlink', uncache)
 
-const server = express()
+/* create an express instance */
 
-server.use('/static', express.static('static'))
-server.use('/build', express.static('.build/dist'))
+const instance = express()
 
-server.use(require('webpack-dev-middleware')(compiler, {
+/* setup hot module replacement */
+
+const compiler = webpack(config)
+
+instance.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }))
 
-server.use(require('webpack-hot-middleware')(compiler))
+instance.use(require('webpack-hot-middleware')(compiler))
 
-server.get('*', (req, res) => {
-  let route = req.path
-  if (route === '/') route = '/home'
+/* handle static files */
+instance.use('/static', express.static('static'))
 
-  let page = route.replace('/', '')
-  if (pages[page]) {
-    const Page = pages[page].default
-    const appString = renderToString(<Page/>)
+/* handle build files */
+instance.use('/build', express.static('.build/dist'))
+
+/* handle routes */
+instance.get('*', (req, res) => {
+  let route = req.path.replace('/', '')
+
+  /* default route */
+  if (route === '') route = 'home'
+
+  /* get page from cached pages */
+  if (pages[route]) {
+    const Page = pages[route].default
+
+    /* get rendered component from ReactDOM */
+    const component = renderToString(<Page/>)
+
+    /* get styles */
     const styles = styleSheet.rules().map(rule => rule.cssText).join('\n')
-    res.send(template(appString, styles, page))
+
+    /* render html page */
+    res.send(template(component, styles, route))
   } else res.status(404).end()
 })
 
-server.listen(3000, () => {
+/* start the instance */
+instance.listen(3000, () => {
   info('SERVER', 'Listening on 3000')
   loading('WATCH', 'Watching for changes...')
 })
--- prod/server.js
+++ prod/server.js
@@ -1,43 +1,65 @@
 import express from 'express'
 import React from 'react'
 import {renderToString} from 'react-dom/server'
-import template from '../utils/template'
 import fs from 'fs'
 import compression from 'compression'
 import webpack from 'webpack'
-import config from './webpack.config'
 import {loading, info} from 'prettycli'
 import {resolve} from 'path'
 import styleSheet from 'styled-components/lib/models/StyleSheet'
 
-const compiler = webpack(config)
+import template from '../utils/template'
+import config from './webpack.config'
 
 loading('BUILD', 'Building...')
+
+/* get webpack compiler */
+const compiler = webpack(config)
+
+/* compile! */
 compiler.run(() => {
+
   loading('PAGES', 'Defining routes...')
+
+  /* generate pages cache */
   let pages = {}
   const files = fs.readdirSync('./pages')
   files.filter(file => file.endsWith('.js'))
   .map(file => pages[file.replace('.js', '')] = require(resolve(`./pages/${file}`)))
 
+  /* create an express server */
   const server = express()
 
+  /* gzip */
   server.use(compression())
+
+  /* handle static files */
   server.use('/static', express.static('static'))
+
+  /* handle build files */
   server.use('/build', express.static('.build/dist'))
 
   server.get('*', (req, res) => {
-    let route = req.path
-    if (route === '/') route = '/home'
+    let route = req.path.replace('/', '')
 
-    let page = route.replace('/', '')
-    if (pages[page]) {
-      const Page = pages[page].default
-      const appString = renderToString(<Page/>)
+    /* default route */
+    if (route === '') route = 'home'
+
+    /* get page from cached pages */
+    if (pages[route]) {
+      const Page = pages[route].default
+
+      /* get rendered component from ReactDOM */
+      const component = renderToString(<Page/>)
+
+      /* get styles */
       const styles = styleSheet.rules().map(rule => rule.cssText).join('\n')
-      res.send(template(appString, styles, page))
+
+      /* render html page */
+      res.send(template(component, styles, route))
     } else res.status(404).end()
   })
 
+  /* start the server */
   server.listen(3000, () => info('SERVER', 'Listening on 3000'))
 })

ac3d98eff2aa5c3cab01b95c341daa63a3de1c3a
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-03-19 15:17:49 +0530

    set env=production if !dev :rocket:

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const dev = process.argv[2] === 'dev'
+if (!dev) process.env.NODE_ENV = 'production';
 
 require('babel-core/register')({
   ignore: /node_modules\/(?!reaqt)/,

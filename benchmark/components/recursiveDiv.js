41fb58581cb53392a66d3e2c87a5d48a04b29edd
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-03-19 19:13:07 +0530

    add benchmarking for prod server

---
+++ benchmark/components/recursiveDiv.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RecursiveDiv = ({length, width}) => {
+  if (length <= 0) return <span>I'm a leaf. </span>
+
+  let children = []
+  for (let i = 0; i < width; i++) {
+    children.push(<RecursiveDiv key={i} length={length-1} width={width}/>)
+  }
+  return <div>{children}</div>
+}
+
+export default RecursiveDiv
---
+++ benchmark/pages/home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import RecursiveDiv from '../components/recursiveDiv'
+
+export default () => {
+  let length = 7;
+  let width = 5;
+  return <RecursiveDiv length={length} width={width}/>
+}

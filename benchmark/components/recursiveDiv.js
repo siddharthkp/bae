6d0c6b26125065c0c24975f987529873c4e4a3f1
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-03-20 00:24:04 +0530

    github syntax highlighting is a baby

--- benchmark/components/recursiveDiv.js
+++ benchmark/components/recursiveDiv.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const RecursiveDiv = ({length, width}) => {
-  if (length <= 0) return <span>I'm a leaf. </span>
+  if (length <= 0) return <span>I am a leaf. </span>
 
   let children = []
   for (let i = 0; i < width; i++) {
